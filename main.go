ff9f0252751bf25e1e2d218e7f5e7d4e3ff0ea22
tsuem <tsuemotop@gmail.com>
2022-09-19 16:43:00 -0300

    Using variables! git commit -m Using variables

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+// Guess the Number Game
+
+const prompt = "and don't type your number in, just press ENTER when ready."
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var firstNumber = rand.Intn(8) + 2
+	var secondNumber = rand.Intn(8) + 2
+	var subtraction = rand.Intn(8) + 2
+	var answer int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// display a welcome and instructions
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	//take them through the game
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	//give them the answer
+	answer = firstNumber*secondNumber - subtraction
+	fmt.Println("The answer is", answer)
+}
